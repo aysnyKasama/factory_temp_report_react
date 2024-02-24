ff0b09e2fd028ce58340a528cd0029d6f3aed6ae
aysnyKasama <75156306+aysnyKasama@users.noreply.github.com>
2024-02-24 17:04:45 +0900

    a

---
+++ src/Views/A4Landscape.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './A4Landscape.css'; // CSSファイルをインポート
+
+const A4Landscape = () => {
+  return (
+    <div className="a4Landscape">
+      
+    </div>
+  );
+}
+
+export default A4Landscape;
--- src/Views/PrintButton.js
+++
@@ -1,9 +0,0 @@
-// src/Views/PrintButton.js
-
-import React from 'react';
-
-function PrintButton() {
-  return <button onClick={() => window.print()}>印刷</button>;
-}
-
-export default PrintButton;
